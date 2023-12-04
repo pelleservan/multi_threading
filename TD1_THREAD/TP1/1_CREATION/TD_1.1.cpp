a016b94d198a401c776a621757c1b67762f2a5a8
pelleservan <pelle.servan@hotmail.fr>
2023-12-04 12:24:22 +0100

    add CREATION 1

--- TD1_THREAD/TP1/1_CREATION/TD_1.1.cpp
+++ TD1_THREAD/TP1/1_CREATION/TD_1.1.cpp
@@ -1,24 +1,25 @@
+// compiler : g++ -std=c++11 -o TD_1.1 TD_1.1.cpp -lpthread
+
 #include <iostream>
 #include <thread>
-#include <vector>
 
-void helloThread(int threadNum) {
-    std::cout << "Hello thread " << threadNum << std::endl;
+// Fonction exécutée par les threads
+void threadFunction(int threadID) {
+    std::cout << "Thread " << threadID << " en cours d'exécution.\n" << std::endl;
 }
 
 int main() {
+    // Créer trois threads
+    std::thread thread1(threadFunction, 1);
+    std::thread thread2(threadFunction, 2);
+    std::thread thread3(threadFunction, 3);
 
-    const int numThreads = 3;
-    
-    std::vector<std::thread> threads;
-    
-    for (int i = 1; i <= numThreads; ++i) {
-        threads.push_back(std::thread(helloThread, i));
-    }
-    
-    for (auto &t : threads) {
-        t.join();
-    }
+    // Attendre que tous les threads aient terminé
+    thread1.join();
+    thread2.join();
+    thread3.join();
+
+    std::cout << "Tous les threads ont terminé." << std::endl;
 
     return 0;
 }
